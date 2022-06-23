7dccc7cd859697fd304c0ac037dd9c5a9a69954e
Arleycht <47849356+Arleycht@users.noreply.github.com>
2022-06-23 07:38:02 -0400

    Experiment with post processing shaders

--- scripts/anthropoid.gd
+++ scripts/anthropoid.gd
@@ -1,9 +1,10 @@
 class_name Anthropoid
 extends CharacterBody3D
 
+
 var box_mover := VoxelBoxMover.new()
 var aabb := AABB(Vector3(-0.5, -0.5, -0.5), Vector3(1, 1, 1))
-var terrain := $"/root/Node3D/VoxelTerrain" as VoxelTerrain
+@onready var terrain := $"/root/Node3D/VoxelTerrain" as VoxelTerrain
 
 
 func _ready() -> void:
