65e611e4fd3079bdabb0b25ed3376263bf59f698
Arleycht <47849356+Arleycht@users.noreply.github.com>
2022-07-07 19:56:44 -0400

    Implement texture stitching

--- scripts/world_loader.gd
+++ scripts/world_loader.gd
@@ -16,6 +16,8 @@ func _init() -> void:
 func _load() -> void:
 	# Load from config
 	
+	library = VoxelBlockyLibrary.new()
+	
 	var modules := _load_modules()
 	var voxel_names := {}
 	var voxels := [
@@ -24,7 +26,7 @@ func _load() -> void:
 			'geometry_type': VoxelBlockyModel.GEOMETRY_NONE,
 		}
 	]
-	var atlas_textures := []
+	var atlas_map := {}
 	var default_mesh = load(DEFAULT_MESH_PATH)
 	
 	# TODO:
@@ -34,143 +36,36 @@ func _load() -> void:
 	# mappings from texture paths to metadata concerning their position and size
 	# in the atlas so that UVs can be mapped to the texture atlas
 	
-	for module in modules.keys():
-		for i in modules[module]['definitions'].size():
-			var def: Dictionary = modules[module]['definitions'][i]
-			var name: String = def['name']
-			var geometry_type: int = VoxelBlockyModel.GEOMETRY_NONE
-			var texture: Texture2D
-			var transparent := false
-			var transparency_index := 0
-			var mesh: Mesh
+	for module_name in modules.keys():
+		for i in modules[module_name]['definitions'].size():
+			var def: Dictionary = modules[module_name]['definitions'][i]
+			var texture_paths: Dictionary = def['textures']
 			
-			if name in voxel_names:
-				printerr("Duplicated voxel name: %s" % name)
-				continue
-			
-			var texture_def := def.get("textures", {}) as Dictionary
-			
-			if texture_def.is_empty():
-				var t := _load_texture(DEFAULT_TEXTURE_PATH)
-				atlas_textures.append({
-					'texture': t,
-					'size': t.get_width(),
-				})
-			elif 'all' in texture_def:
-				var p := "res://modules/%s/textures/%s" % [module, texture_def['all']]
-				var t := _load_texture(p)
-				t.get_image().save_png("res://.temp/test.png")
-				
-				# Append to atlas_textures with metadata describing texture
-				# size so that UVs can be calculated in stitching step
-				pass
-			else:
-				for side in texture_def:
-					print(side)
-			
-#			if File.file_exists(texture_path):
-#				var image_texture := ImageTexture.new()
-#				var image := Image.new()
-#				image.convert(image.FORMAT_RGBA8)
-#				image.load(texture_path)
-#				image_texture.create_from_image(image)
-#				texture = image_texture
-#				print("Loaded texture at \"%s\"" % texture_path)
-#			else:
-#				printerr("Could not find texture at \"%s\"" % texture_path)
-			
-			match (def.get("geometry_type", "cube") as String).to_lower():
-				"none":
-					geometry_type = VoxelBlockyModel.GEOMETRY_NONE
-					mesh = null
-				"cube":
-					geometry_type = VoxelBlockyModel.GEOMETRY_CUBE
-					mesh = _build_cube_mesh()
-				"mesh":
-					geometry_type = VoxelBlockyModel.GEOMETRY_CUSTOM_MESH
-			
-			var mesh_path = def.get("mesh", null)
-			
-			if mesh_path != null and File.file_exists(mesh_path):
-				mesh = load(mesh_path) as Mesh
-				print("Loaded mesh at \"%s\"" % mesh_path)
-			elif mesh_path != null:
-				printerr("Could not find mesh at \"%s\"" % mesh_path)
-			
-			if def.has("transparent"):
-				transparent = def.get("transparent", false) as bool
-			
-			if def.has("transparency_index"):
-				transparency_index = def.get("transparency_index", 0) as int
-			
-			var data := {
-				'name': name,
-				'geometry_type': geometry_type,
-				'texture_index': texture,
-				'transparent': transparent,
-				'transparency_index': transparency_index,
-				'mesh': mesh,
-			}
-			
-			voxel_names[name] = data
-			voxels.append(data)
-			atlas_textures.append(texture)
+			for file_name in texture_paths.values():
+				var texture_id := "%s/%s" % [module_name, file_name]
+				var path := _get_texture_path(module_name, file_name)
+				var image := _load_image(path)
+				atlas_map[texture_id] = {
+					'image': image,
+					'rect': Rect2i(0, 0, image.get_width(), image.get_height()),
+				}
 	
 	# Stitch textures
 	
+	var size := _pack_atlas(atlas_map)
+	
 	var atlas_image := Image.new()
-	var atlas := ImageTexture.new()
-	var atlas_resolution := 64
-	var atlas_size := atlas_textures.size()
+	atlas_image.create(size.x, size.y, false, Image.FORMAT_RGBA8)
 	
-	atlas_image.create(atlas_resolution * atlas_size * 6, atlas_resolution,
-			false, Image.FORMAT_RGBA8)
+	for texture_id in atlas_map:
+		var img := atlas_map[texture_id]['image'] as Image
+		var src_rect := Rect2(Vector2(), img.get_size())
+		var dst := atlas_map[texture_id]['rect'].position as Vector2i
+		atlas_image.blit_rect(atlas_map[texture_id]['image'], src_rect, dst)
 	
-	for i in atlas_textures.size():
-		var texture = atlas_textures[i] as Texture2D
-		var src_image := texture.get_image()
-		src_image.convert(Image.FORMAT_RGBA8)
-		src_image.resize(atlas_resolution, atlas_resolution)
-		var src := Rect2(Vector2(), Vector2.ONE * atlas_resolution)
-		var dest := Vector2(i * atlas_resolution, 0)
-		atlas_image.blit_rect(src_image, src, dest)
+	var atlas_texture := ImageTexture.new()
+	atlas_texture.create_from_image(atlas_image)
 	
-	atlas_image.save_png("res://test.png")
-	atlas.create_from_image(atlas_image)
-	
-	# Create voxels in library
-	
-	library = VoxelBlockyLibrary.new()
-	materials = []
-	
-	library.voxel_count = voxels.size()
-	library.atlas_size = atlas_size
-	
-	var default_material := StandardMaterial3D.new()
-	default_material.vertex_color_use_as_albedo = true
-	default_material.albedo_texture = atlas
-	materials.append(default_material)
-	
-	for id in voxels.size():
-		var data := voxels[id]
-		var voxel := library.create_voxel(id, data['name'])
-		voxel.geometry_type = VoxelBlockyModel.GEOMETRY_NONE
-	
-		if 'mesh' in data:
-			voxel.geometry_type = VoxelBlockyModel.GEOMETRY_CUSTOM_MESH
-			voxel.custom_mesh = data['mesh']
-	
-		if 'transparent' in data:
-			voxel.transparent = data['transparent']
-	
-		if 'transparency_index' in data:
-			voxel.transparency_index = data['transparency_index']
-	
-		voxel.set_material_override(0, materials[0])
-	
-		print("Created voxel \"%s\"" % data['name'])
-	
-	# Bake mesh data
 	library.bake()
 
 
@@ -221,7 +116,11 @@ func _load_definition_file(file_path: String) -> Dictionary:
 	return {}
 
 
-func _load_texture(path: String) -> Texture2D:
+func _get_texture_path(module_name: String, file_name: String) -> String:
+	return "res://modules/%s/textures/%s" % [module_name, file_name]
+
+
+func _load_image(path: String) -> Image:
 	if path == null or not File.file_exists(path):
 		path = DEFAULT_TEXTURE_PATH
 	
@@ -230,11 +129,29 @@ func _load_texture(path: String) -> Texture2D:
 	img.load(path)
 	img.convert(Image.FORMAT_RGBA8)
 	
-	var s := max(img.get_width(), img.get_height()) as int
-	img.resize(s, s)
+	return img
+
+
+func _pack_atlas(atlas_map: Dictionary) -> Vector2i:
+	var min_pos := Vector2i()
+	var max_pos := Vector2i()
 	
-	texture.create_from_image(img)
-	return texture
+	# TODO: Use a more optimal rectangle packing strategy
+	# Maps to a straight line, suboptimal if rectangles differ in size
+	var x := 0
+	for k in atlas_map:
+		var data: Dictionary = atlas_map[k]
+		var rect := data['rect'] as Rect2i
+		rect.position.x = x
+		x += rect.size.x
+		
+		atlas_map[k]['rect'] = rect
+		min_pos.x = min(min_pos.x, rect.position.x)
+		min_pos.y = min(min_pos.y, rect.position.y)
+		max_pos.x = max(max_pos.x, rect.position.x + rect.size.x)
+		max_pos.y = max(max_pos.y, rect.position.y + rect.size.y)
+	
+	return max_pos - min_pos
 
 
 func _build_cube_mesh(p: float = 0.0, q: float = 1.0 / 6.0) -> ArrayMesh:
